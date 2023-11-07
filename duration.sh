49fc4cf609ed252a52c9799e54ef10d4ca710d92
lderequesens <leonardo.sobarzo@protonmail.com>
2023-11-07 11:25:54 -0300

    Add transform script

--- duration.sh
+++ duration.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-for file in *.mkv; do
-    in="${file}"
-    out="${file%.mkv}.mp4"
-    ffmpeg -i ${in} -codec:a aac -b:a 128k -codec:v libx264 -crf 23 ${out}
-    if [ $? -eq 0 ]; then
-        rm ${in}
-    fi
+total_hours=0
+total_minutes=0
+total_seconds=0
+
+for file in *.mp4; do
+    duration=$(ffmpeg -hide_banner -i $file 2>&1 | grep Duration | cut -d ',' -f1 | awk '{print $2}')
+    IFS=':' read -r hours minutes seconds <<< $duration
+
+    ((total_hours+=hours))
+    ((total_minutes+=minutes))
+    seconds=$(bc <<< "$seconds / 1")
+    ((total_seconds+=seconds))
 done
 
+((total_minutes+=total_seconds / 60))
+((total_seconds=total_seconds % 60))
+((total_hours+=total_minutes / 60))
+((total_minutes=total_minutes % 60))
+
+echo "Time used: ${total_hours}:${total_minutes}:${total_seconds}"
