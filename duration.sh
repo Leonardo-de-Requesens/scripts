6b8031604f4fddfef6fac97213e3b6a6f59aa32e
lderequesens <leonardo.sobarzo@protonmail.com>
2024-01-08 15:50:38 -0300

    Add m4a audio files and not throw error with wildcards

--- duration.sh
+++ duration.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+shopt -s nullglob # Filename globbing patterns that don't match any filenames are simply expanded to nothing
+
 total_hours=0
 total_minutes=0
 total_seconds=0
 
 for file in *.mp4; do
 	duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal $file)
+	IFS=':' read -r hours minutes seconds <<< $duration
+
+	((total_hours+=hours))
+	((total_minutes+=minutes))
+	seconds=$(bc <<< "$seconds / 1")
+	((total_seconds+=seconds))
+done
+
+for audio in *.m4a; do
+	duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal $audio)
 	IFS=':' read -r hours minutes seconds <<< $duration
 
 	((total_hours+=hours))
