671f13f926ccd5ab5ebb69114d37e40b56a712a6
lderequesens <leonardo.sobarzo@protonmail.com>
2024-02-04 11:28:54 -0300

    No more errors when m4a aren't found

--- duration.sh
+++ duration.sh
@@ -7,7 +7,10 @@ total_minutes=0
 total_seconds=0
 
 for file in *{.mp4,.m4a}; do
-	duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal $file)
+	duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal $file 2>/dev/null)
+	if [ $? -ne 0 ]; then
+		continue
+	fi
 	IFS=':' read -r hours minutes seconds <<< $duration
 
 	((total_hours+=hours))
